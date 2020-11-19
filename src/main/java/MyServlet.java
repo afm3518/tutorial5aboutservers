e48ccd720e78b79188ac9bc2e01e0f1ec8256d38
afm3518 <alexis.morgan18@imperial.ac.uk>
2020-11-19 09:16:49 +0000

    ;jhgv

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -13,7 +13,7 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         resp.setContentType("text/html");
-        resp.getWriter().write("Hello, world prout!");
+        resp.getWriter().write("Hello, world!");
 
 
     }
